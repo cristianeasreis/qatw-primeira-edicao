c77fc932ac48bcf1e2ef07d3298e02f286b870e2
Cristiane A S Reis <cristianeasreis@hotmail.com>
2025-02-13 01:33:18 -0300

    aula 2 concluida

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,57 +1,44 @@
-import { test, expect } from '@playwright/test';
-import { obterCodigo2FA } from '../support/db';
+import { test, expect } from "@playwright/test";
+import { obterCodigo2FA } from "../support/db";
+import { LoginPage } from "../pages/LoginPage";
 
-test('Não deve logar quando o codigo de autenticação é invalido', async ({ page }) => {
+test("Não deve logar quando o codigo de autenticação é invalido", async ({
+  page,
+}) => {
+  const loginPage = new LoginPage(page);
+
   const usuario = {
-    cpf: '00000014141',
-    senha: '147258'
-  }
+    cpf: "00000014141",
+    senha: "147258",
+  };
+  await loginPage.acessaPagina();
+  await loginPage.informaCPF(usuario.cpf);
+  await loginPage.informaSenha(usuario.senha);
+  await loginPage.informaCodigo2FA("123456");
 
-  await page.goto('http://localhost:3000/');
-
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  for(const digito of usuario.senha) {
-    await page.getByRole('button', { name: digito }).click();
-   }
-
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
-
-  await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+  await expect(page.locator("span")).toContainText(
+    "Código inválido. Por favor, tente novamente."
+  );
 });
 
-test('Deve acessar a conta do usuario', async ({ page }) => {
+test("Deve acessar a conta do usuario", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
   const usuario = {
-    cpf: '00000014141',
-    senha: '147258'
-  }
+    cpf: "00000014141",
+    senha: "147258",
+  };
+  await loginPage.acessaPagina();
+  await loginPage.informaCPF(usuario.cpf);
+  await loginPage.informaSenha(usuario.senha);
 
-  await page.goto('http://localhost:3000/');
-
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  for(const digito of usuario.senha) {
-    await page.getByRole('button', { name: digito }).click();
-   }
-
-  await page.getByRole('button', { name: 'Continuar' }).click();
-  
   // temporario
   await page.waitForTimeout(2000);
 
-  const code = await obterCodigo2FA();
-
-  await page.getByRole('textbox', { name: '000000' }).fill(code);
-  await page.getByRole('button', { name: 'Verificar' }).click();
-
+  const codigo = await obterCodigo2FA();
+  await loginPage.informaCodigo2FA(codigo);
+  // temporario
   await page.waitForTimeout(2000);
 
-  await expect (page.locator('#account-balance')).toHaveText('R$ 5.000,00');
-
-
+ expect(await loginPage.obterSaldo()).toHaveText('R$ 5.000,00');
 });
