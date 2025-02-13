15c5635ce7d2b99daef6beaf34f2a7a7b25badbe
Cristiane A S Reis <cristianeasreis@hotmail.com>
2025-02-12 22:56:30 -0300

    ajustando os testes de acordo com video

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,19 +1,19 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('validate that my login fails when code is invalid', async ({ page }) => {
-  const user = {
+test('Não deve logar quando o codigo de autenticação é invalido', async ({ page }) => {
+  const usuario = {
     cpf: '00000014141',
-    password: '147258'
+    senha: '147258'
   }
 
   await page.goto('http://localhost:3000/');
 
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(user.cpf);
+  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
   await page.getByRole('button', { name: 'Continuar' }).click();
 
-  for(const digit of user.password) {
-    await page.getByRole('button', { name: digit }).click();
+  for(const digito of usuario.senha) {
+    await page.getByRole('button', { name: digito }).click();
    }
 
   await page.getByRole('button', { name: 'Continuar' }).click();
@@ -21,5 +21,28 @@ test('validate that my login fails when code is invalid', async ({ page }) => {
   await page.getByRole('textbox', { name: '000000' }).fill('123456');
   await page.getByRole('button', { name: 'Verificar' }).click();
 
-  // await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+  await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+});
+
+test('Deve acessar a conta do usuario', async ({ page }) => {
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258'
+  }
+
+  await page.goto('http://localhost:3000/');
+
+  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
+  await page.getByRole('button', { name: 'Continuar' }).click();
+
+  for(const digito of usuario.senha) {
+    await page.getByRole('button', { name: digito }).click();
+   }
+
+  await page.getByRole('button', { name: 'Continuar' }).click();
+
+  await page.getByRole('textbox', { name: '000000' }).fill('315269');
+  await page.getByRole('button', { name: 'Verificar' }).click();
+
+
 });
