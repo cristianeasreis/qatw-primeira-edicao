9221e1ec057c89d0acdacd5838106ac6ce6117f5
Cristiane A S Reis <cristianeasreis@hotmail.com>
2025-02-13 22:59:05 -0300

    aula de redis concluida

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,6 +3,8 @@ import { obterCodigo2FA } from "../support/db";
 import { LoginPage } from "../pages/LoginPage";
 import { DashPage } from "../pages/DashPage";
 
+import { cleanJobs, getJob } from "../support/redis";
+
 test("Não deve logar quando o codigo de autenticação é invalido", async ({
   page,
 }) => {
@@ -30,17 +32,21 @@ test("Deve acessar a conta do usuario", async ({ page }) => {
     cpf: "00000014141",
     senha: "147258",
   };
+
+  await cleanJobs()
+
   await loginPage.acessaPagina();
   await loginPage.informaCPF(usuario.cpf);
   await loginPage.informaSenha(usuario.senha);
 
-  // temporario
-  await page.waitForTimeout(2000);
+  await page.getByRole('heading', { name: 'Verificação em duas etapas' })
+    .waitFor({ timeout: 3000 });
+   
+  const codigo  = await getJob();
+  
 
-  const codigo = await obterCodigo2FA();
+  // const codigo = await obterCodigo2FA(usuario.cpf);
   await loginPage.informaCodigo2FA(codigo);
-  // temporario
-  await page.waitForTimeout(2000);
 
- expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00');
+  expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00');
 });
